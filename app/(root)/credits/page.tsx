c294259312b945b30f39773b2cb35d555212e792
kumarvivek-777 <coding1172001@gmail.com>
2024-07-01 00:34:18 +0530

    initial commit

---
+++ app/(root)/credits/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreditPage() {
+  return (
+    <div>
+      creaditpage
+    </div>
+  )
+}
+
+export default CreditPage
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TransformationPage() {
+  return (
+    <div>
+      transformationpage
+    </div>
+  )
+}
+
+export default TransformationPage
